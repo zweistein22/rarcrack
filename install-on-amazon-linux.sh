39343989cae8e891d8bd02c10b9cfea5990c7d11
zweistein22 <andreas.langhoff@gmail.com>
2024-11-05 21:58:26 +0100

    b3

---
+++ install-on-amazon-linux.sh
@@ -0,0 +1,4 @@
+yum install gcc-c++
+yum install make glibc-devel gcc patch
+dnf install -y cmake
+sudo yum install libxml2-devel
---
+++ install-on-ubuntu.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install -y build-essential g++
+sudo apt install -y cmake pkg-config
+sudo apt install -y libxml2-dev
+sudo apt-get install git
+git clone https://github.com/zweistein22/rarcrack.git
+cd rarcrack
+cmake .
+cd bin
